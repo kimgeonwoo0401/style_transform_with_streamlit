bec33a29d1dcd061612d918d2fe0792a7bf20dd8
kimgeonwoo0401 <124033576+kimgeonwoo0401@users.noreply.github.com>
2023-01-31 17:32:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def image_to_byte(img):
     img.save(buffer, format='JPEG')
     return buffer
 
-# unsafe_allow_html: html 태그를 넣을 수 있음
-st.write("# Style transfer")
-st.write("### AI를 이용한 나만의 웹어플!")
+# 제목
+st.write('# 이미지 결합')
+st.write('## 이미지 선택하기')
 
 # 사이드 바 만들기
 models = os.listdir(root_model)
@@ -36,7 +36,7 @@ else:
 # 스타일을 바꿀 사진 업로드
 img = None
 uploaded_file = st.file_uploader(
-    "이미지를 골라주세요", type=["jpg", "jpeg"], key = 'img')
+    "파일업로드", type=["jpg", "jpeg"], key = 'img')
 
 show_file = st.empty()
 
@@ -47,7 +47,7 @@ if uploaded_file:
     img.save(input_path, "JPEG")
     
     # 업로드한 이미지 띄워주기
-    st.image(img, caption='내가 올린 이미지', use_column_width=True)
+    st.image(img, caption='나의 이미지', use_column_width=True)
     
     # 이미 학습된 스타일을 선택하면 스타일 이미지 띄우기
     if path_style != None:
@@ -56,24 +56,24 @@ if uploaded_file:
     else:
         img2 = None
         uploaded_st_file = st.file_uploader(
-            "입히고 싶은 이미지를 골라주세요.", type=["jpg", "jpeg"], key = 'style')
+            "선택한 이미지", type=["jpg", "jpeg"], key = 'style')
         
         # 스타일 이미지가 업로드 됐는지 확인
         if uploaded_st_file:
             img2 = Image.open(uploaded_st_file)
             img2.save(style_path, "JPEG")
-            st.image(img2, caption='내가 올린 스타일 이미지', use_column_width=True)
+            st.image(img2, caption='스타일 이미지', use_column_width=True)
             
             # 학습할 때 사용할 이미지들의 크기 선택
             img_size = st.select_slider(
-                    '이미지 사이즈를 골라주세요.',
+                    '이미지 사이즈 선택하기',
                     options=[8, 16, 32, 64, 128, 256, 512])
-            st.write('이미지 사이즈가 크면 결과는 좋아질 수 있지만 학습이 오래 걸리거나 학습이 되지 않을 수 있습니다.')
+            st.write('이미지 사이즈가 크면 결과는 좋아질 수 있지만 학습이 오래 걸리거나 학습이 되지 않을 수 있습니다.( 추천 사이즈 32)')
             # 학습 횟수 선택
             epoch = st.select_slider(
-                    '학습 횟수를 골라주세요',
-                    options=[1, 2, 3, 4, 5])
-            st.write('학습 횟수가 늘어나면 결과는 좋아질 수도 있지만 그만큼 학습이 오래 걸립니다.')
+                    '학습 횟수 선택하기',
+                    options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+            st.write('학습 횟수가 늘어나면 결과는 좋아질 수도 있지만 그만큼 학습이 오래 걸립니다.(추천 학습 힛수 3~5)')
             
             
 extensions = [".png", ".jpeg", ".jpg"]
@@ -89,11 +89,10 @@ if uploaded_file is not None and any(extension in uploaded_file.name for extensi
 
     root_output = "./images/sample"
 
-    stylize_button = st.button("Stylize")
+    stylize_button = st.button("stylize")
     
     if path_style == None:
-        st.warning("Streamlit 배포 환경에서는 학습하는 것을 권장하지 않습니다(약 10시간 이상 필요). PC에서 local로 진행해 주시기 바랍니다.")
-          
+        st.warning
     if stylize_button:
         try:
             if path_style == None:
@@ -108,7 +107,7 @@ if uploaded_file is not None and any(extension in uploaded_file.name for extensi
             st.image(stylized, width=400, use_column_width=True)
             # 다운로드 버튼을 눌러 만들어진 이미지 내려받기
             st.download_button(
-                label="Download Image",
+                label="download image",
                 data=image_to_byte(stylized),
                 file_name=f"{style_name}_{name_file[0]}.jpg"
             )
